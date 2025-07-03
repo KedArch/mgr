da9492203559afae96ffae293c9b043870c87f06
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-07-03 12:21:52 +0200

    redirect to service 2153

--- ansible/roles/manifests/files/reg/scripts/srscu.sh
+++ ansible/roles/manifests/files/reg/scripts/srscu.sh
@@ -3,4 +3,5 @@ cp /etc/srscu.yaml.in /etc/srscu.yaml
 sed -i "s/AMF_ADDR/$AMF_ADDR/g" /etc/srscu.yaml
 CONTAINER_INTERNAL_IP="$(ip a show eth0 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f 3 | cut -d '/' -f 1)"
 sed -i "s/CORE_BIND_ADDR/$CONTAINER_INTERNAL_IP/g" /etc/srscu.yaml
+sed -i "s/F1U_EXT_ADDR/$SRSCU_F1U_SERVICE_SERVICE_HOST/g" /etc/srscu.yaml
 /usr/bin/srscu -c /etc/srscu.yaml

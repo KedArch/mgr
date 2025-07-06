161dcedcffc24e0928689959696c75199c266a18
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-07-06 14:30:43 +0200

    modify scripts

--- ansible/roles/manifests/templates/regions/reg/scripts/srscu.sh
+++ ansible/roles/manifests/templates/regions/reg/scripts/srscu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp /etc/srscu.yaml.in /etc/srscu.yaml
+cp /etc/srscu.yaml{.in,}
 sed -i "s/AMF_ADDR/$AMF_ADDR/g" /etc/srscu.yaml
 CONTAINER_INTERNAL_IP="$(ip a show eth0 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f 3 | cut -d '/' -f 1)"
 sed -i "s/CORE_BIND_ADDR/$CONTAINER_INTERNAL_IP/g" /etc/srscu.yaml

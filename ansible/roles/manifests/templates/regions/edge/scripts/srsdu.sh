3dcad09fcde8da51e5a9120dbd660d8a6a04e4bf
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-07-06 13:29:35 +0200

    manifests reduce quotation marks, remove -service and -network-policy from names, merge files for one app into one file

--- ansible/roles/manifests/templates/regions/edge/scripts/srsdu.sh
+++ ansible/roles/manifests/templates/regions/edge/scripts/srsdu.sh
@@ -2,5 +2,5 @@
 cp /etc/srsdu.yaml.in /etc/srsdu.yaml
 sed -i "s/CU_CP_ADDR/$CU_CP_ADDR/g" /etc/srsdu.yaml
 sed -i "s/UE_ADDR/$UE_ADDR/g" /etc/srsdu.yaml
-sed -i "s/F1U_EXT_ADDR/$SRSDU_F1U_SERVICE_SERVICE_HOST/g" /etc/srsdu.yaml
+sed -i "s/F1U_EXT_ADDR/$SRSDU_F1U_SERVICE_HOST/g" /etc/srsdu.yaml
 /usr/bin/srsdu -c /etc/srsdu.yaml

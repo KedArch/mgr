b9e1dbdb54d8549def6b9b9fb2c7ee3873eaf531
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-07-06 14:41:11 +0200

    remove iproute2 dependency

--- ansible/roles/manifests/templates/regions/reg/scripts/srscu.sh
+++ ansible/roles/manifests/templates/regions/reg/scripts/srscu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp /etc/srscu.yaml{.in,}
 sed -i "s/AMF_ADDR/$AMF_ADDR/g" /etc/srscu.yaml
-CONTAINER_INTERNAL_IP="$(ip a show eth0 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f 3 | cut -d '/' -f 1)"
+CONTAINER_INTERNAL_IP="$(hostname -I)"
 sed -i "s/CORE_BIND_ADDR/$CONTAINER_INTERNAL_IP/g" /etc/srscu.yaml
 sed -i "s/F1U_EXT_ADDR/$SRSCU_F1U_SERVICE_HOST/g" /etc/srscu.yaml
 iptables -t nat -A POSTROUTING -p udp --sport 2152 -s 127.0.0.1 -j SNAT --to-source $CONTAINER_INTERNAL_IP

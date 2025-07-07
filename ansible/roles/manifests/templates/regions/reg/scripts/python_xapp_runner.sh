75064fd9b18280c9a4007ba9271968871da4bfbb
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-07-07 12:22:26 +0200

    XAPP_PY_RUNNER_IP fix in python_xapp_runner

--- ansible/roles/manifests/templates/regions/reg/scripts/python_xapp_runner.sh
+++ ansible/roles/manifests/templates/regions/reg/scripts/python_xapp_runner.sh
@@ -2,8 +2,8 @@
 cp /opt/ric/config/uta-rtg.rt{.in,}
 CONTAINER_INTERNAL_IP="$(hostname -I)"
 export RMR_SRC_ID=$CONTAINER_INTERNAL_IP
-sed -i "s/DBAAS_IP/$DBAAS_SERVICE_HOST/g" /opt/ric/config/uta-rtg.rt
 sed -i "s/E2MGR_IP/$E2MGR_SERVICE_HOST/g" /opt/ric/config/uta-rtg.rt
 sed -i "s/E2TERM_IP/$E2TERM_SERVICE_HOST/g" /opt/ric/config/uta-rtg.rt
 sed -i "s/SUBMGR_IP/$SUBMGR_SERVICE_HOST/g" /opt/ric/config/uta-rtg.rt
+sed -i "s/XAPP_PY_RUNNER_IP/$PYTHON_XAPP_RUNNER_SERVICE_HOST/g" /opt/ric/config/uta-rtg.rt
 /bin/bash
